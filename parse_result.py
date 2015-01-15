e0a9c825ac9f732e03fd6fa6f47e2912f5b5919b
Jinglei Ren <jinglei@ren.systems>
2015-01-15 15:06:41 -0800

    Tweaks the output parsing script parse_result.py.

--- parse_result.py
+++ parse_result.py
@@ -1,60 +1,69 @@
-#!/usr/bin/python
+#! /usr/bin/env python
 
 import sys
 import os
 import numpy
 
-# Main
+__author__ = "Jinglei Ren"
+__copyright__ = "Copyright (c) 2014 Jinglei Ren"
+__email__ = "jinglei@ren.systems"
 
-if len(sys.argv) != 2:
-    print "Usage: %s FileName" % sys.argv[0]
-    sys.exit(-1)
 
-path = sys.argv[1]
+def main():
+    if len(sys.argv) != 2 or sys.argv[1] == '-h':
+        print "Usage: %s OutputFileName" % sys.argv[0]
+        print "Statistics (.result file) for each workload " + \
+              "will be written to the workload directory."
+        sys.exit(-1)
 
-lines = [line.strip().split('\t') for line in open(path)]
+    path = sys.argv[1]
 
-results = { }
+    lines = [line.strip().split('\t') for line in open(path)]
 
-db_index = set() # db name
-tn_index = set() # thread number
+    results = {}
 
-for line in lines:
-    if line[0][0] == '#':
-        continue
-    db_name = line[0]
-    db_index.add(db_name)
+    db_index = set() # db name
+    tn_index = set() # thread number
 
-    dir_name, workload = os.path.split(line[1])
-    workload = os.path.splitext(workload)[0]
+    for line in lines:
+        if line[0][0] == '#':
+            continue
+        db_name = line[0]
+        db_index.add(db_name)
 
-    num_threads = int(line[2])
-    tn_index.add(num_threads)
+        dir_name, workload = os.path.split(line[1])
+        workload = os.path.splitext(workload)[0]
 
-    throughput = float(line[3])
-    if not results.has_key(workload):
-        results[workload] = { }
-    if not results[workload].has_key(db_name):
-        results[workload][db_name] = { }
-    if not results[workload][db_name].has_key(num_threads):
-        results[workload][db_name][num_threads] = [ ]
-    results[workload][db_name][num_threads].append(throughput)
+        num_threads = int(line[2])
+        tn_index.add(num_threads)
 
-db_index = sorted(db_index)
-tn_index = sorted(tn_index)
+        throughput = float(line[3])
+        if not results.has_key(workload):
+            results[workload] = {}
+        if not results[workload].has_key(db_name):
+            results[workload][db_name] = {}
+        if not results[workload][db_name].has_key(num_threads):
+            results[workload][db_name][num_threads] = []
+        results[workload][db_name][num_threads].append(throughput)
 
-for wl in sorted(results.keys()):
-    out_file = open(os.path.join(dir_name, wl + ".result"), 'w+')
-    # Prints header
-    line = "#"
-    for db in db_index:
-        line += '\t' + db
-    out_file.write(line + '\n')
-    # Prints results
-    for tn in tn_index:
-        line = str(tn)
+    db_index = sorted(db_index)
+    tn_index = sorted(tn_index)
+
+    for wl in sorted(results.keys()):
+        out_file = open(os.path.join(dir_name, wl + ".result"), 'w+')
+        # Prints header
+        line = "#"
         for db in db_index:
-            data = results[wl][db][tn]
-            line += '\t' + str(numpy.median(numpy.array(data)))
+            line += '\t' + db
         out_file.write(line + '\n')
+        # Prints results
+        for tn in tn_index:
+            line = str(tn)
+            for db in db_index:
+                data = results[wl][db][tn]
+                line += '\t' + str(numpy.median(numpy.array(data)))
+            out_file.write(line + '\n')
+
+if __name__ == '__main__':
+    main()
 
